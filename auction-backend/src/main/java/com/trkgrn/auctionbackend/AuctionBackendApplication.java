4e36f03ff479b354e643a9d43bc3db1b1634c2e8
trkgrn <takoyaso58@gmail.com>
2023-03-21 19:13:25 +0300

    project started

---
+++ auction-backend/src/main/java/com/trkgrn/auctionbackend/AuctionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trkgrn.auctionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionBackendApplication.class, args);
+    }
+
+}
---
+++ auction-backend/src/test/java/com/trkgrn/auctionbackend/AuctionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trkgrn.auctionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
